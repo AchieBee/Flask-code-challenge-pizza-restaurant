c62b68a8664eca0978b6c03131f6fe2fb2557bbb
AchieBee <belza.achieng@student.moringaschool.combelza.achieng@student.moringaschool.com>
2023-10-30 16:59:57 +0300

    feat:initializing data with faker

--- server/models.py
+++ server/models.py
@@ -2,33 +2,30 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-
 class Restaurant(db.Model):
-    __tablename__ = 'restaurant'
+    __tablename__ = 'restaurants'
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     address = db.Column(db.String(200), nullable=False)
-    pizzas = db.relationship('RestaurantPizza', backref='restaurant', cascade='all, delete-orphan')
-
+    restaurant_pizzas = db.relationship('RestaurantPizza', backref='restaurant', cascade='all, delete-orphan')
 
 class Pizza(db.Model):
-    __tablename__ = 'pizza'
+    __tablename__ = 'pizzas'
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     ingredients = db.Column(db.String(200), nullable=False)
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now()) 
-    restaurants = db.relationship('RestaurantPizza', backref='pizza', cascade='all, delete-orphan')
-
+    restaurant_pizzas = db.relationship('RestaurantPizza', backref='pizza', cascade='all, delete-orphan')
 
 class RestaurantPizza(db.Model):
-    __tablename__ = 'restaurant_pizza'
+    __tablename__ = 'restaurant_pizzas'
 
     id = db.Column(db.Integer, primary_key=True)
     price = db.Column(db.Float, nullable=False)
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
-    restaurant_id = db.Column(db.Integer, db.ForeignKey('restaurant.id'), nullable=False)
-    pizza_id = db.Column(db.Integer, db.ForeignKey('pizza.id'), nullable=False)
+    restaurant_id = db.Column(db.Integer, db.ForeignKey('restaurants.id'), nullable=False)
+    pizza_id = db.Column(db.Integer, db.ForeignKey('pizzas.id'), nullable=False)
